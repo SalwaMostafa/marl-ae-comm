5d0b497b137a5940ab3f9653f7e06fbafe7bdd13
Salwa Mostafa <37854037+SalwaMostafa@users.noreply.github.com>
2022-09-28 10:32:05 +0200

    Update ops.py

--- marl-grid/find-goal/util/ops.py
+++ marl-grid/find-goal/util/ops.py
@@ -42,8 +42,8 @@ def img_to_state_var(x, use_gpu=True, dtype=np.float32):
     return var.cuda() if use_gpu else var
 
 
-def to_torch(x.cpu(), use_gpu=True, dtype=np.float32):
-    x = np.array(x, dtype=dtype)
+def to_torch(x, use_gpu=True, dtype=np.float32):
+    x = np.array(x.cpu(), dtype=dtype)
     var = torch.from_numpy(x)
     return var.cuda() if use_gpu else var
 
