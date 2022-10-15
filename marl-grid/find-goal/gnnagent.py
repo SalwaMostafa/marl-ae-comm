4d4c0be4b4bac808da1d6290dd484fdcdb0f1f67
Salwa Mostafa <37854037+SalwaMostafa@users.noreply.github.com>
2022-10-15 09:17:52 +0200

    Update gnnagent.py

--- marl-grid/find-goal/gnnagent.py
+++ marl-grid/find-goal/gnnagent.py
@@ -18,7 +18,7 @@ def parse_code(net_code: str):
     return nb_gnn_layers, nb_dense_layers, is_max
 
 class GNNAgent(nn.Module):
-    def __init__(self, obj_n: int, action_n: int, input_dims:List[int], type: str, embedding_size=16, net_code="2g0f", mp_rounds=1):
+    def __init__(self, obj_n: int, input_dims:List[int], embedding_size=16, net_code="2g0f", mp_rounds=1):
         super().__init__()
         nb_edge_types = input_dims[2]
         nb_layers, nb_dense_layers, self.max_reduce = parse_code(net_code)
@@ -27,27 +27,7 @@ class GNNAgent(nn.Module):
         for i in range(nb_layers):
             gnn_layers.append(RGCNConv(embedding_size, embedding_size, nb_edge_types))
         self.gnn_layers = nn.ModuleList(gnn_layers)
-        dense_layers = []
-        for i in range(nb_dense_layers):
-            if i == 0:
-                if self.max_reduce:
-                    dense_layers.append(nn.Linear(embedding_size, 128))
-                else:
-                    dense_layers.append(nn.Linear(embedding_size*obj_n, 128))
-            else:
-                dense_layers.append(nn.Linear(128, 128))
-            dense_layers.append(nn.ReLU())
-        self.dense = nn.Sequential(*dense_layers)
-        self.num_actions = action_n
-        if nb_dense_layers == 0:
-            self.policy_linear = nn.Linear(embedding_size, self.num_actions)
-            self.baseline_linear = nn.Linear(embedding_size, 1)
-        else:
-            self.policy_linear = nn.Linear(128, self.num_actions)
-            self.baseline_linear = nn.Linear(128, 1)
-        self.mp_rounds = mp_rounds
-        self.nb_dense_layers = nb_dense_layers
-
+                
     def forward(self, obs, core_state=()):
         T, B, *_ = obs["unary_tensor"].shape
         device=next(self.parameters()).device
@@ -73,19 +53,5 @@ class GNNAgent(nn.Module):
             x, _ = torch.max(x, dim=1)
         else:
             x = torch.flatten(x, start_dim=1, end_dim=2)
-        x = self.dense(x)
-        policy_logits = self.policy_linear(x)
-        baseline = self.baseline_linear(x)
-        if self.training:
-            action = torch.multinomial(F.softmax(policy_logits, dim=1), num_samples=1)
-        else:
-            # Don't sample when testing.
-            action = torch.argmax(policy_logits, dim=1)
-        policy_logits = policy_logits.view(T, B, self.num_actions)
-        baseline = baseline.view(T, B)
-        action = action.view(T, B)
-        if self.training:
-            return dict(policy_logits=policy_logits, baseline=baseline, action=action)
-        else:
-            return dict(policy_logits=policy_logits, baseline=baseline, action=action)
-
+        return x 
+        
